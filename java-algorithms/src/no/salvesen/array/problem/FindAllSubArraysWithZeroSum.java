2fc576949eb6e234c74a13815285404b54f695a3
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2020-05-23 08:15:30 +0200

    Add a solver for finding all sub arrays with zero sum. Most likely faster ways to accomplish.

---
+++ java-algorithms/src/no/salvesen/array/problem/FindAllSubArraysWithZeroSum.java
@@ -0,0 +1,48 @@
+package no.salvesen.array.problem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllSubArraysWithZeroSum {
+
+  /**
+   * http://www.techiedelight.com/find-sub-array-with-0-sum/
+   * Problem: Given an array of integers, print all sub-arrays with 0 sum.
+   */
+  public static void main(String[] args) {
+    int[] arr = {4, 2, -3, -1, 0, 4};
+    int[] arr2 = {3, 4, -7, 3, 1, 3, 1, -4, -2, -2};
+
+
+    List<int[]> result = solve(arr2);
+
+    result.stream().map(Arrays::toString).forEach(System.out::println);
+  }
+
+
+  private static List<int[]> solve(int[] arr) {
+    List<int[]> subArrs = new ArrayList<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      int sum = arr[i];
+
+      if (sum == 0) {
+        subArrs.add(new int[]{arr[i]});
+      }
+
+      List<Integer> sumArr = new ArrayList<>();
+      sumArr.add(arr[i]);
+      for (int j = i + 1; j < arr.length; j++) {
+        sum += arr[j];
+        sumArr.add(arr[j]);
+        if (sum == 0) {
+          int[] array = sumArr.stream().mapToInt(e -> e).toArray();
+          subArrs.add(array);
+        }
+      }
+    }
+
+    return subArrs;
+  }
+}
